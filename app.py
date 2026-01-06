c7a734ef4d1490e224d129964180faf4816de592
boo99ooq <giusmasala99@gmail.com>
2026-01-06 10:26:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,109 +1,309 @@
-import streamlit as st
-import pandas as pd
+Hai perfettamente ragione, mi scuso per l'errore precedente. Stavo guardando una versione non aggiornata dei dati.
 
-# 1. Configurazione della pagina
-st.set_page_config(page_title="Fantalega Manageriale", layout="wide")
-st.title("⚽ La mia Fantalega Manageriale")
+Ho ricaricato il file più recente (v11) e confermo che ora vedo i dati corretti:  - **Vlahovic** è nella squadra di **GIUSEPPE** al prezzo di **141**.
+  - Ho applicato la sostituzione del nome della squadra da FABIO a **NICHOLAS**.Ecco il file CSV aggiornato e corretto basato su questi dati.``` csv
+Ruolo,Fantasquadra,Nome,Prezzo
+Portiere,GIANNI,di gregorio,11.0
+Portiere,DANI ROBI,BUTEZ,15.0
+Portiere,MARCO,CARNESECCHI,16.0
+Portiere,PIETRO,LEALI,6.0
+Portiere,PIERLUIGI,Maignan,22.0
+Portiere,GIGI,PROVEDEL,21.0
+Portiere,ANDREA,MERET,45.0
+Portiere,GIUSEPPE,Sommer,34.0
+Portiere,MATTEO,DE GEA,30.0
+Portiere,NICHOLAS,Svilar,40.0
+Portiere,GIANNI,PERIN,1.0
+Portiere,DANI ROBI,MONTIPO,1.0
+Portiere,MARCO,MANDAS,3.0
+Portiere,PIETRO,SIEGRIST,1.0
+Portiere,PIERLUIGI,TERRACCIANO,1.0
+Portiere,GIGI,SKORUPSKI,17.0
+Portiere,ANDREA,MILINKOVIC SAVIC,35.0
+Portiere,GIUSEPPE,Martinez,1.0
+Portiere,MATTEO,MARTINELLI,1.0
+Portiere,NICHOLAS,ISRAEL,8.0
+Portiere,GIANNI,PINSOGLIO,1.0
+Portiere,DANI ROBI,okoye,1.0
+Portiere,MARCO,CAPRILE,5.0
+Portiere,PIETRO,SOMMARIVA,1.0
+Portiere,PIERLUIGI,TORRIANI,1.0
+Portiere,GIGI,RAVAGLIA,1.0
+Portiere,ANDREA,contini,1.0
+Portiere,GIUSEPPE,muric,1.0
+Portiere,MATTEO,SEMPER,1.0
+Portiere,NICHOLAS,SAVA,2.0
+Portiere,MARCO,SPORTIELLO,1.0
+Portiere,GIGI,FURLANETTO,1.0
+Portiere,GIUSEPPE,TURATI,1.0
+Portiere,MATTEO,AUDERO,1.0
+Portiere,NICHOLAS,FALCONE,2.0
+Difensore,GIANNI,de winter 2002,15.0
+Difensore,DANI ROBI,Cambiaso,5.0
+Difensore,MARCO,KEMPF,1.0
+Difensore,PIETRO,bremer,16.0
+Difensore,PIERLUIGI,Kalulu,1.0
+Difensore,GIGI,di lorenzo,15.0
+Difensore,ANDREA,NDICKA,7.0
+Difensore,GIUSEPPE,ROMAGNOLI,4.0
+Difensore,MATTEO,buongiorno,17.0
+Difensore,NICHOLAS,Bellanova,40.0
+Difensore,GIANNI,MARTIN,15.0
+Difensore,DANI ROBI,thiago gabriel,1.0
+Difensore,MARCO,GOSENS,31.0
+Difensore,PIETRO,ANGELINO,13.0
+Difensore,PIERLUIGI,HERMOSO,4.0
+Difensore,GIGI,BASCHIROTTO,5.0
+Difensore,ANDREA,LUCUMI,5.0
+Difensore,GIUSEPPE,SCALVINI,1.0
+Difensore,MATTEO,DI MARCO,40.0
+Difensore,NICHOLAS,NORTON CUFFY,6.0
+Difensore,GIANNI,MARIPAN,1.0
+Difensore,DANI ROBI,VALERI,7.0
+Difensore,MARCO,SOLET,9.0
+Difensore,PIETRO,AKANJI,8.0
+Difensore,PIERLUIGI,HEGGEM,1.0
+Difensore,GIGI,PALESTRA,9.0
+Difensore,ANDREA,PAVLOVIC,7.0
+Difensore,GIUSEPPE,LUPERTO,6.0
+Difensore,MATTEO,GABBIA,3.0
+Difensore,NICHOLAS,PEZZELLA,2.0
+Difensore,GIANNI,RRAHMANI,6.0
+Difensore,DANI ROBI,KRISTENSEN,3.0
+Difensore,MARCO,TERRACCIANO,4.0
+Difensore,PIETRO,DODO,15.0
+Difensore,PIERLUIGI,MIRANDA,10.0
+Difensore,GIGI,ZAPPACOSTA,11.0
+Difensore,ANDREA,MARUSIC,3.0
+Difensore,GIUSEPPE,GILA,4.0
+Difensore,MATTEO,ANGORI,11.0
+Difensore,NICHOLAS,ESTUPINAN,18.0
+Difensore,GIANNI,RENSCH,2.0
+Difensore,DANI ROBI,BASTONI,12.0
+Difensore,MARCO,VOJVODA,9.0
+Difensore,PIETRO,WESLEY,28.0
+Difensore,PIERLUIGI,KOUSSOUNOU,1.0
+Difensore,GIGI,GATTI,6.0
+Difensore,ANDREA,bartesaghi,15.0
+Difensore,GIUSEPPE,NUNO TAVARES,20.0
+Difensore,MATTEO,COMUZZO,3.0
+Difensore,NICHOLAS,DUMFRIES,41.0
+Difensore,GIANNI,Kamara H.,1.0
+Difensore,DANI ROBI,ZAPPA,3.0
+Difensore,MARCO,VITIK,4.0
+Difensore,PIETRO,KELLY,1.0
+Difensore,PIERLUIGI,MUHAREMOVIC,1.0
+Difensore,GIGI,DOIG,4.0
+Difensore,ANDREA,MANCINI,12.0
+Difensore,GIUSEPPE,BEUKEMA,2.0
+Difensore,MATTEO,RANIERI,1.0
+Difensore,NICHOLAS,DELPRATO,9.0
+Difensore,GIANNI,COCO,1.0
+Difensore,DANI ROBI,ZANOLI,3.0
+Difensore,MARCO,GUTIERREZ,1.0
+Difensore,PIETRO,MINA,7.0
+Difensore,PIERLUIGI,RAMON,15.0
+Difensore,GIGI,BARBIERI,1.0
+Difensore,ANDREA,ACERBI,1.0
+Difensore,GIUSEPPE,HIEN,8.0
+Difensore,MATTEO,DARMIAN,4.0
+Difensore,NICHOLAS,FRESE,4.0
+Difensore,GIANNI,VASQUEZ,5.0
+Difensore,DANI ROBI,CARLOS AUGU,4.0
+Difensore,MARCO,GALLO,2.0
+Difensore,PIETRO,OBERT,1.0
+Difensore,PIERLUIGI,BERTOLA,7.0
+Difensore,GIGI,PONGRACIC,1.0
+Difensore,ANDREA,DJIMSITI,1.0
+Difensore,GIUSEPPE,zortea,5.0
+Difensore,MATTEO,CUADRADO,8.0
+Difensore,NICHOLAS,HOLM,8.0
+Difensore,GIANNI,GASPAR,2.0
+Difensore,DANI ROBI,POSCH,4.0
+Difensore,MARCO,TOMORI,1.0
+Difensore,PIETRO,ZIOLKOWSKI,1.0
+Difensore,PIERLUIGI,IDZES,1.0
+Difensore,GIGI,CELIK,1.0
+Difensore,ANDREA,JUAN JESUS,4.0
+Difensore,GIUSEPPE,OSTIGAARD,2.0
+Difensore,MATTEO,SPINAZZOLA,7.0
+Difensore,NICHOLAS,LAZARO,5.0
+Centrocampista,GIANNI,Ederson,21.0
+Centrocampista,DANI ROBI,Conceicao,31.0
+Centrocampista,MARCO,Samardzic,1.0
+Centrocampista,PIETRO,Prati,7.0
+Centrocampista,PIERLUIGI,de roon,9.0
+Centrocampista,GIGI,Zaniolo,30.0
+Centrocampista,ANDREA,K THURAM,30.0
+Centrocampista,GIUSEPPE,Frattesi,15.0
+Centrocampista,MATTEO,da cunha,18.0
+Centrocampista,NICHOLAS,Barella,27.0
+Centrocampista,GIANNI,Zaccagni,54.0
+Centrocampista,DANI ROBI,perrone,17.0
+Centrocampista,MARCO,CALHANOGLU,60.0
+Centrocampista,PIETRO,Odgaard,1.0
+Centrocampista,PIERLUIGI,Gudmundsson,37.0
+Centrocampista,GIGI,ZIELINSKI,1.0
+Centrocampista,ANDREA,MODRIC,35.0
+Centrocampista,GIUSEPPE,ferguson,1.0
+Centrocampista,MATTEO,Lovric,13.0
+Centrocampista,NICHOLAS,nicolussi caviglia,2.0
+Centrocampista,GIANNI,Maldini 2001,3.0
+Centrocampista,DANI ROBI,POBEGA,1.0
+Centrocampista,MARCO,Brescianini,30.0
+Centrocampista,PIETRO,Ricci,20.0
+Centrocampista,PIERLUIGI,loftus cheek,27.0
+Centrocampista,GIGI,nico paz,24.0
+Centrocampista,ANDREA,ZHEGROVA,40.0
+Centrocampista,GIUSEPPE,Neres,74.0
+Centrocampista,MATTEO,DE BRUYNE,151.0
+Centrocampista,NICHOLAS,felici,5.0
+Centrocampista,GIANNI,RABIOT,42.0
+Centrocampista,DANI ROBI,TRAMONI,30.0
+Centrocampista,MARCO,Koopmeiners,20.5
+Centrocampista,PIETRO,FOLORUNSHO,20.0
+Centrocampista,PIERLUIGI,Fofana,25.0
+Centrocampista,GIGI,Mctominay,43.0
+Centrocampista,ANDREA,FAGIOLI,14.0
+Centrocampista,GIUSEPPE,THORSVEDT,13.0
+Centrocampista,MATTEO,HARROUI,11.0
+Centrocampista,NICHOLAS,vlasic,23.0
+Centrocampista,GIANNI,POLITANO,41.0
+Centrocampista,DANI ROBI,FRENDRUP,3.0
+Centrocampista,MARCO,orsolini,42.0
+Centrocampista,PIETRO,Anguissa,31.0
+Centrocampista,PIERLUIGI,BATURINA,30.0
+Centrocampista,GIGI,Saelemakers,12.0
+Centrocampista,ANDREA,ELMAS,9.0
+Centrocampista,GIUSEPPE,CASADEI,1.0
+Centrocampista,MATTEO,ILIC,1.0
+Centrocampista,NICHOLAS,KONE SASSUOLO,1.0
+Centrocampista,GIANNI,L.PELLEGRINI,55.0
+Centrocampista,DANI ROBI,PASALIC,35.0
+Centrocampista,MARCO,Malinovskyi,18.0
+Centrocampista,PIETRO,VAZQUEZ,22.0
+Centrocampista,PIERLUIGI,VENTURINO,7.0
+Centrocampista,GIGI,pulisic,85.0
+Centrocampista,ANDREA,LOBOTKA,1.0
+Centrocampista,GIUSEPPE,PIEROTTI,2.0
+Centrocampista,MATTEO,SUCIC,10.0
+Centrocampista,NICHOLAS,TOURE,6.0
+Centrocampista,GIANNI,CRISTANTE,13.0
+Centrocampista,DANI ROBI,ORISTANIO,13.0
+Centrocampista,MARCO,ISAKSEN,51.0
+Centrocampista,PIETRO,GUENDOUZI,18.0
+Centrocampista,PIERLUIGI,LOCATELLI,3.0
+Centrocampista,GIGI,ZALEWSKI,11.0
+Centrocampista,ANDREA,M KONE,15.0
+Centrocampista,GIUSEPPE,BAILEY,1.0
+Centrocampista,MATTEO,BONDO,1.0
+Centrocampista,NICHOLAS,DELE BASHIRU,7.0
+Centrocampista,DANI ROBI,EKKELENKAMP,2.0
+Centrocampista,MARCO,DOUGLAS LUIZ,12.0
+Centrocampista,GIGI,SUSLOV,3.0
+Centrocampista,MATTEO,MARIN,2.0
+Centrocampista,MATTEO,MESSIAS,1.0
+Attaccante,GIANNI,lukaku,150.0
+Attaccante,DANI ROBI,dybala,115.0
+Attaccante,MARCO,thuram,208.0
+Attaccante,PIETRO,RETEGUI,132.0
+Attaccante,PIERLUIGI,leao,108.0
+Attaccante,GIGI,castellanos,30.0
+Attaccante,ANDREA,morata,85.0
+Attaccante,GIUSEPPE,lautaro,230.0
+Attaccante,MATTEO,lookman,121.0
+Attaccante,NICHOLAS,kvaratskhelia,145.0
+Attaccante,GIANNI,castro,21.0
+Attaccante,DANI ROBI,dallinga,6.0
+Attaccante,MARCO,KEAN,66.0
+Attaccante,PIETRO,dovbyk,120.0
+Attaccante,PIERLUIGI,soulè,61.0
+Attaccante,GIGI,abraham,32.0
+Attaccante,ANDREA,TENGSTEDT,22.0
+Attaccante,GIUSEPPE,Vlahovic,141.0
+Attaccante,MATTEO,YILDIZ,17.5
+Attaccante,NICHOLAS,dia,36.0
+Attaccante,GIANNI,beltran,11.0
+Attaccante,DANI ROBI,bonny,26.0
+Attaccante,MARCO,raspadori,10.0
+Attaccante,PIETRO,LUCCA,15.0
+Attaccante,PIERLUIGI,zapata,52.0
+Attaccante,GIGI,mosquera,11.0
+Attaccante,ANDREA,GIMENEZ,62.0
+Attaccante,GIUSEPPE,TAREMI,26.0
+Attaccante,MATTEO,SCAMACCA,20.0
+Attaccante,NICHOLAS,krstovic,9.0
+Attaccante,GIANNI,ljuvic,2.0
+Attaccante,DANI ROBI,pinamonti,21.0
+Attaccante,MARCO,cutrone,18.0
+Attaccante,PIETRO,NOSLIN,13.0
+Attaccante,PIERLUIGI,piccoli,1.0
+Attaccante,GIGI,pojanphalo,20.0
+Attaccante,ANDREA,OKAFOR,1.0
+Attaccante,GIUSEPPE,sanabria,2.0
+Attaccante,MATTEO,DE KETELAERE,16.0
+Attaccante,NICHOLAS,vitinha,1.0
+Attaccante,GIANNI,DURIC,1.0
+Attaccante,DANI ROBI,davis,1.0
+Attaccante,MARCO,sancho,22.0
+Attaccante,PIETRO,ADAMS,8.0
+Attaccante,PIERLUIGI,benedyczak,1.0
+Attaccante,GIGI,bandeira,1.0
+Attaccante,ANDREA,JOVIC,1.0
+Attaccante,GIUSEPPE,CORREA,1.0
+Attaccante,MATTEO,MIHAILA,1.0
+Attaccante,NICHOLAS,BRENNER,1.0
+Attaccante,GIANNI,CAPRARI,1.0
+Attaccante,DANI ROBI,SHOMURODOV,1.0
+Attaccante,MARCO,PETAGNA,1.0
+Attaccante,PIETRO,LIVRAMENTO,1.0
+Attaccante,PIERLUIGI,shpendi,1.0
+Attaccante,GIGI,relic,1.0
+Attaccante,ANDREA,SANCHEZ,1.0
+Attaccante,GIUSEPPE,SIMEONE,1.0
+Attaccante,MATTEO,ARNAUTOVIC,1.0
+Attaccante,NICHOLAS,MURIEL,1.0
+Giovani,GIANNI,LIBERATO,1.0
+Giovani,DANI ROBI,MBUYI,1.0
+Giovani,MARCO,KUHN,1.0
+Giovani,PIETRO,SADIKU,1.0
+Giovani,PIERLUIGI,LIBERALI,1.0
+Giovani,GIGI,anghele,1.0
+Giovani,ANDREA,CAMARDA,1.0
+Giovani,GIUSEPPE,BANDA,1.0
+Giovani,MATTEO,VAVASSORI,1.0
+Giovani,NICHOLAS,iliev,1.0
+Giovani,GIANNI,VIGNATO,1.0
+Giovani,DANI ROBI,jimenez,1.0
+Giovani,MARCO,Diao,51.0
+Giovani,PIETRO,SANGARE,1.0
+Giovani,PIERLUIGI,pafundi,1.0
+Giovani,GIGI,v. carboni,1.0
+Giovani,ANDREA,ADOPO,1.0
+Giovani,GIUSEPPE,Miretti 2003,1.0
+Giovani,MATTEO,KOUAME,1.0
+Giovani,NICHOLAS,DOMINGUEZ,1.0
+Giovani,GIANNI,PISILLI,1.0
+Giovani,DANI ROBI,cancellieri,1.0
+Giovani,MARCO,JASHARI,1.0
+Giovani,PIETRO,EBUHEI,1.0
+Giovani,PIERLUIGI,SAVONA,1.0
+Giovani,GIGI,braf,1.0
+Giovani,ANDREA,ZEROLI,1.0
+Giovani,GIUSEPPE,Gnonto 2003,1.0
+Giovani,MATTEO,BELAHYANE,1.0
+Giovani,NICHOLAS,ALIDOU,1.0
+Giovani,DANI ROBI,DAHL,1.0
+Giovani,MARCO,vlahovic ata 2004,1.0
+Giovani,PIERLUIGI,rouhi,1.0
+Giovani,GIUSEPPE,Baldanzi,19.0
+Giovani,MATTEO,GINEITIS,1.0
+Giovani,DANI ROBI,IDRISSI,1.0
+Giovani,MARCO,Fabbian,1.0
+Giovani,PIETRO,Semedo,1.0
+Giovani,GIUSEPPE,fazzini,1.0
+Giovani,GIGI,AMBROSINO,1.0
+Giovani,MARCO,cisse,1.0
+Giovani,NICHOLAS,CREMASCHI,1.0
 
-# 2. Definizione dei Budget di Febbraio
-# Nota: ho aggiornato i nomi in MAIUSCOLO per combaciare con il nuovo file
-# "Dany Roby" del file precedente ora è "DANI ROBI"
-budgets_fisso = {
-    "GIANNI": 164,
-    "DANI ROBI": 162,
-    "MARCO": 194,
-    "PIETRO": 164,
-    "PIERLUIGI": 240,
-    "GIGI": 222,
-    "ANDREA": 165,
-    "GIUSEPPE": 174,
-    "MATTEO": 166,
-    "NICHOLAS": 162
-}
-
-# 3. Caricamento file nella barra laterale
-st.sidebar.header("Gestione Dati")
-file_caricato = st.sidebar.file_uploader("Carica il file: nuove rose muynov25.csv", type="csv")
-
-if file_caricato is not None:
-    try:
-        # Lettura flessibile del file
-        file_caricato.seek(0)
-        df = pd.read_csv(file_caricato, sep=None, engine='python', encoding='utf-8')
-        
-        # PULIZIA DATI
-        df = df.dropna(how='all').reset_index(drop=True)
-        df.columns = df.columns.str.strip() # Toglie spazi dai nomi delle colonne
-        
-        # Mappatura per adattare il nuovo file (Nome -> Calciatore, Prezzo -> Valore Rosa)
-        mappa_colonne = {
-            'Nome': 'Calciatore',
-            'Prezzo': 'Valore Rosa'
-        }
-        df = df.rename(columns=mappa_colonne)
-
-        # Uniformiamo i nomi delle squadre (tutto maiuscolo e senza spazi extra)
-        if 'Fantasquadra' in df.columns:
-            df['Fantasquadra'] = df['Fantasquadra'].astype(str).str.strip().str.upper()
-
-        # Convertiamo il valore della rosa in numero
-        if 'Valore Rosa' in df.columns:
-            df['Valore Rosa'] = pd.to_numeric(df['Valore Rosa'], errors='coerce').fillna(0)
-
-        # Creazione delle Tab
-        tab1, tab2 = st.tabs(["📊 Potenziale Economico", "🏃 Dettaglio Rose"])
-
-        with tab1:
-            st.subheader("Analisi Finanziaria Febbraio")
-            
-            # Calcolo valore attuale delle rose
-            analisi = df.groupby('Fantasquadra')['Valore Rosa'].sum().reset_index()
-            
-            # Aggiungiamo il Budget di Febbraio incrociando i nomi
-            analisi['Budget Febbraio'] = analisi['Fantasquadra'].map(budgets_fisso).fillna(0)
-            
-            # Calcolo Potenziale Totale
-            analisi['Potenziale Totale'] = analisi['Valore Rosa'] + analisi['Budget Febbraio']
-            analisi = analisi.sort_values(by='Potenziale Totale', ascending=False)
-            
-            col_graf, col_tab = st.columns([2, 1.5])
-            with col_graf:
-                st.write("### Composizione (Rosa + Budget)")
-                # Grafico a barre sovrapposte
-                st.bar_chart(data=analisi, x='Fantasquadra', y=['Valore Rosa', 'Budget Febbraio'])
-            with col_tab:
-                st.write("### Riepilogo Crediti")
-                st.dataframe(analisi, hide_index=True, use_container_width=True)
-
-        with tab2:
-            squadre_disponibili = sorted([s for s in df['Fantasquadra'].unique() if pd.notna(s)])
-            scelta = st.selectbox("Scegli una Fantasquadra:", squadre_disponibili)
-            
-            rosa_squadra = df[df['Fantasquadra'] == scelta].copy()
-            budget_team = budgets_fisso.get(scelta, 0)
-            valore_team = rosa_squadra['Valore Rosa'].sum()
-            
-            # Visualizzazione delle metriche principali
-            c1, c2, c3 = st.columns(3)
-            c1.metric("Valore Rosa Attuale", f"{int(valore_team)}")
-            c2.metric("Budget Febbraio", f"{int(budget_team)}")
-            c3.metric("Potenziale d'acquisto", f"{int(valore_team + budget_team)}")
-
-            # Ordine colonne: Ruolo per primo, poi Nome e Prezzo
-            ordine_visualizzazione = ['Ruolo', 'Calciatore', 'Valore Rosa']
-            
-            # Ordinamento logico dei ruoli (Portieri -> Difensori -> Centrocampisti -> Attaccanti)
-            ordine_ruoli = {'Portiere': 1, 'Difensore': 2, 'Centrocampista': 3, 'Attaccante': 4, 'Giovani': 5}
-            rosa_squadra['sort_key'] = rosa_squadra['Ruolo'].map(ordine_ruoli).fillna(99)
-            
-            st.dataframe(
-                rosa_squadra.sort_values(by='sort_key')[ordine_visualizzazione], 
-                use_container_width=True, 
-                hide_index=True
-            )
-
-    except Exception as e:
-        st.error(f"Si è verificato un errore: {e}")
-        st.info("Assicurati di aver caricato il file 'nuove rose muynov25.csv'")
-else:
-    st.info("👋 Benvenuto! Carica il file aggiornato delle rose per visualizzare la situazione della lega.")
+```
